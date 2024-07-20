7e372838404e722fd01124336bbd7c326b374b3f
sunny <150616454+myaongE@users.noreply.github.com>
2024-07-21 01:08:38 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        
+        long answer = A+B+C;
+        
+        System.out.println(answer);
+    
+    }
+}
