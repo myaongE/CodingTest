62ce542b0ab1366b337f07caeb4d4e71d34c3d99
sunny <150616454+myaongE@users.noreply.github.com>
2024-07-21 00:17:08 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        
+        double answer = (double)num1/(double)num2;
+        
+        System.out.println(answer);
+        
+    }
+}
