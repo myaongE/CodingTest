294e2cb1e3f2456a86d8787ebf46d089710b60ab
sunny <150616454+myaongE@users.noreply.github.com>
2024-07-21 03:50:35 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int r = 0;
+
+        for(int i=1; i<=n; i++){
+            r += i;
+        }
+        System.out.println(r);
+ }
+}
