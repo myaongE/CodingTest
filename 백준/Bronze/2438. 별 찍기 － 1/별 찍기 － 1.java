ec74428aee19638d0b55c13d8c33a71d1ead3491
sunny <150616454+myaongE@users.noreply.github.com>
2024-07-21 15:27:14 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int t = sc.nextInt();
+      String r = "";
+
+      for(int i=1; i<t+1; i++){
+          
+       r += "*";
+          
+        System.out.println(r);
+      }
+    }
+}
