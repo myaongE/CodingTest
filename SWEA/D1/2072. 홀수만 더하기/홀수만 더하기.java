01b6398e21533e3237504c7a2964ca39f3c445e9
sunny <150616454+myaongE@users.noreply.github.com>
2025-09-29 15:36:26 +0900

    [D1] Title: 홀수만 더하기, Time: 92 ms, Memory: 26,240 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int sum = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                int a = sc.nextInt();
+                if(a % 2 == 1)
+                {
+                    sum += a;
+                }
+            }
+            System.out.printf("#%d %d\n", test_case, sum);
+        }
+    }
+}
